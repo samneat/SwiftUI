9fb799b76ac91b1f351de6c1a0ca216c887c8c10
Sam Neat <samjneat@gmail.com>
2024-07-16 09:55:49 +0100

    Create MapView and import MapKit. create region

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,27 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Sam Neat on 16/07/2024.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    
+    private var region: MKCoordinateRegion {
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    }
+    
+    var body: some View {
+        Map(initialPosition: .region(region))
+    }
+}
+
+#Preview {
+    MapView()
+}
